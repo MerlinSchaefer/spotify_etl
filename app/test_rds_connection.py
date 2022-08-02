5edf5ef5e52423418179cd43f86439c25e7182a0
MerlinSchaefer <48853130+MerlinSchaefer@users.noreply.github.com>
2022-08-02 16:44:11 +0200

    implement database connection with boto3 token

--- app/test_rds_connection.py
+++ app/test_rds_connection.py
@@ -1,16 +1,30 @@
 import psycopg2
 import os
-
-db_user = os.getenv('DB_USERNAME')
-db_password = os.getenv('DB_PASSWORD')
-db_host = os.getenv("DB_HOST")
-db_name = os.getenv("DB_NAME")
+import boto3
 
 
+DB_USER = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+PORT="5432"
+REGION = "eu-central-1"
 
-connection = psycopg2.connect(f"dbname={db_name} user={db_user} password={db_password} host={db_host} port=5432")
 
-cursor=connection.cursor()
+session = boto3.Session(profile_name='default')
+client = session.client('rds')
 
-print(connection)
-print(cursor)
+token = client.generate_db_auth_token(DBHostname=DB_HOST, Port=PORT, DBUsername=DB_USER, Region=REGION)
+try:
+    connection = psycopg2.connect(f"dbname={DB_NAME} user={DB_USER} password={DB_PASSWORD} host={DB_HOST} port={PORT}")
+
+    cursor=connection.cursor()
+
+    print(connection)
+    print(cursor)
+    cursor.execute("""SELECT now()""")
+    query_results = cursor.fetchall()
+    print(query_results)
+    print("Database connection established.")
+except Exception as e:
+    print(f"Database connection failed due to {e}")   
