95929e97253f4594a8ea282c893456b45ba83dcc
MerlinSchaefer <48853130+MerlinSchaefer@users.noreply.github.com>
2023-01-06 13:29:39 +0100

    rework montly playlist date calc

--- monthly_playlist.py
+++ monthly_playlist.py
@@ -1,7 +1,7 @@
 import psycopg2
 import os
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 from app.configuration import set_spotify_variables, set_postgres_variables
 from app.authentication import authenticate
 
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, PORT, REGION = set_postgres_variables()
     print("Variables set")
     # Get last month
-    last_month = datetime.now().month - 1
-    current_year = datetime.now().year
+    last_month = (datetime.now() - timedelta(days=30)).month
+    current_year = (datetime.now() - timedelta(days=30)).year
     print(f"Creating Playlist 'Flashback {current_year}-{last_month}'")
     # Authenticate with Spotify API
     SCOPE = SCOPE + " playlist-modify-private playlist-read-private"
